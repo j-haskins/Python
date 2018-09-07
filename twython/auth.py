f4dc4c0c777f84f49f319fc036294f460ba8ee52
j-haskins <jay.haskins@gmail.com>
2018-09-07 12:22:56 -0700

    adding twython /twitter api key file - no creds included

---
+++ twython/auth.py
@@ -0,0 +1,4 @@
+consumer_key        = 'consumer_key from apps.twitter.com'
+consumer_secret     = 'consumer_secret from apps.twitter.com'
+access_token        = 'access_token from apps.twitter.com'
+access_token_secret = 'access_token_scret from apps.twitter.com'
